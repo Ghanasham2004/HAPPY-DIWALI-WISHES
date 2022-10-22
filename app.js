7f4e10b4d9931d3433761fc6d684d6e56d195613
Ghansham Irashetti <105876844+itsSham143@users.noreply.github.com>
2022-10-22 19:27:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -62,7 +62,6 @@ let quotes = [
     "Let us celebrate the Diwali festival with smiles on our faces and joy in our hearts.” “Diwali greetings to everyone.",
     "May the allure of Diwali never wane. “Let’s celebrate this great holiday with the best foods and company.” Happy Diwali!",
     "'May the Diwali festive season be filled with celebrations, festivities, and great zeal.'I wish everyone a happy Diwali.'",
-    "Happy birthday! Hope you have a warm day, I'm thinking of you always, and I'll see you soon!",
     "May Diwali bring you enough light to chase away all the darkness in your life.",
     "Let's add another lovely memory to our life by celebrating Diwali with our families and friends. A lovely Diwali to you",
     "May the beauty of the Deepawali season fill your home with joy, and may the coming year bring you everything that makes you happy.",
